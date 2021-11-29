5b696564027d4249a60781b9ead793b82e055eb9
James Meegan <jmeegan2@owlmail.harford.edu>
2021-11-29 02:40:49 -0500

    Initial commit

---
+++ src/ForLoops.java
@@ -0,0 +1,19 @@
+public class ForLoops {
+
+    public static void main(String[] args) {
+
+      for(int i = 0; i < 59000; i++) {
+          System.out.println("I love dogs!");
+      }
+
+      for(int i = 0; i <= 15; i++) {
+          System.out.println(i);
+      }
+
+      int[] grades = {98, 100, 83, 90, 93};
+
+      for(int i = 0; i < grades.length;i++) {
+          System.out.println(grades[i]);
+      }
+    }
+}
